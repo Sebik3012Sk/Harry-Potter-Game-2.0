2c26e429869a8491a0ec29e3486d3573c2278c07
Sebik3012Sk <sebik-kucera@hotmail.com>
2022-10-24 14:56:11 +0200

    CREATED FILES Enemy.py , Game.py

---
+++ Enemy.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+import time
+
+pygame.init()
+
+WIDTH = 1200
+HEIGHT = 650
+
+class ShadowEnemy(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.image.load("img/mozkomor.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.x = random.choice([-1,1])
+        self.y = random.choice([-1,1])
+        self.speed_enemy = 6
+        
+
+    def update(self):
+
+        self.rect.x += self.x * self.speed_enemy
+        self.rect.y += self.y * self.speed_enemy
+
+        if self.rect.left < 0 or self.rect.right > WIDTH:
+            self.x= -1 * self.x
+            time.sleep(0.001)
+        
+        if self.rect.top < 100 or self.rect.bottom > HEIGHT - 100:
+            self.y = -1 * self.y
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
-import time
+
+from Game import Main
+from Enemy import ShadowEnemy
 
 pygame.init()
 
@@ -24,35 +26,7 @@ magic_voice.set_volume(0.2)
 fps = 60
 clock = pygame.time.Clock()
 
-class Main(pygame.sprite.Sprite):
-    def __init__(self,x_pos,y_pos):
-        super().__init__()
-        self.image = pygame.image.load("img/bg-dementors.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = (x_pos,y_pos)
-
-class ShadowEnemy(pygame.sprite.Sprite):
-    def __init__(self,x,y):
-        super().__init__()
-        self.image = pygame.image.load("img/mozkomor.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = (x,y)
-        self.x = random.choice([-1,1])
-        self.y = random.choice([-1,1])
-        self.speed_enemy = 6
-        
-
-    def update(self):
-
-        self.rect.x += self.x * self.speed_enemy
-        self.rect.y += self.y * self.speed_enemy
-
-        if self.rect.left < 0 or self.rect.right > WIDTH:
-            self.x= -1 * self.x
-            time.sleep(0.001)
-        
-        if self.rect.top < 100 or self.rect.bottom > HEIGHT - 100:
-            self.y = -1 * self.y
+    
 
 class Player(pygame.sprite.Sprite and ShadowEnemy):
     def __init__(self,x,y,group_of_enemy):
